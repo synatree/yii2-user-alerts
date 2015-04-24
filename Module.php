bc5cf08962448e2376f7a61023b6391af4f08479
David Baltusavich <david@synatree.com>
2015-04-24 15:49:03 +0000

    using dynamic relations as the base

---
+++ Module.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace synatree\useralerts;
+
+class Module extends \yii\base\Module
+{
+	public $layout = "bare";
+	public $basePath = "@app/vendor/synatree/yii2-user-alerts";
+}
---
+++ SynatreeAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace synatree\useralerts;
+
+use yii\web\AssetBundle;
+
+class SynatreeAsset extends AssetBundle {
+
+	public $sourcePath = '@vendor/synatree/yii2-user-alerts/assets';
+	public $js = [
+		'js/user-alerts.js'
+	];
+}
